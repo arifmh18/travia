603f0e0958d2bf28acfd4ebd3571c81fd96e000b
Andi <andi502.as@gmail.com>
2020-08-24 21:00:08 +0700

    add LoginActivity.kt and finishing interface, add custom font (secular_one_regular.ttf), add assets from figma (ic_vector_login_lower.xml, ic_vector_login_upper.xml)

---
+++ app/src/main/java/com/travia/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.travia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_login)
+    }
+}
