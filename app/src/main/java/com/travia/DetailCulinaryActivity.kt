3fa2d5cc1d1c14891247868031578ac50e6d80c9
Andi <andi502.as@gmail.com>
2020-08-28 20:39:34 +0700

    add collapsing toolbar at activity_detail_culinary.xml

---
+++ app/src/main/java/com/travia/DetailCulinaryActivity.kt
@@ -0,0 +1,11 @@
+package com.travia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailCulinaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_culinary)
+    }
+}
