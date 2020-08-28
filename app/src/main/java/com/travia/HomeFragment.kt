1da2c642747ff4d59850008d1dd4fc7851515239
Andi <andi502.as@gmail.com>
2020-08-28 18:35:12 +0700

    add carousel view at HomeFragment.kt

--- app/src/main/java/com/travia/HomeFragment.kt
+++ app/src/main/java/com/travia/HomeFragment.kt
@@ -5,23 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.synnapps.carouselview.ImageListener
+import com.travia.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentHomeBinding
 
-    }
+    private val sampleImage = arrayOf(
+        R.drawable.wisata_pujon_kidul, 
+        R.drawable.bromo_tengger_semeru_national_park, 
+        R.drawable.lawang_sewu
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+            carouselViewHome.pageCount = sampleImage.size
+            carouselViewHome.setImageListener(imageListener)
+        }
+    }
+
+    var imageListener = ImageListener{position: Int, imageView: ImageView? ->
+        imageView?.setImageResource(sampleImage[position])
     }
 
     companion object {
-        var TAG = HomeFragment::class.java.simpleName
     }
 }
