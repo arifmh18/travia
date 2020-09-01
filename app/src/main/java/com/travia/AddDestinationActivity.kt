c26d93b629532c91102a4c536583bbe339efe1ea
Andi <andi502.as@gmail.com>
2020-09-01 20:07:29 +0700

    add general information about destination at AddDestinationActivity.kt

--- app/src/main/java/com/travia/AddDestinationActivity.kt
+++ app/src/main/java/com/travia/AddDestinationActivity.kt
@@ -3,13 +3,20 @@ package com.travia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.travia.databinding.ActivityAddDestinationBinding
+import com.travia.utils.LoadingDialogUtil
 
 class AddDestinationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddDestinationBinding
 
-//    private lateinit var database:
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var loadingDialogUtil: LoadingDialogUtil
 
     private val destinationCategory = listOf("Wisata Kota", "Wisata Alam")
 
@@ -23,6 +30,12 @@ class AddDestinationActivity : AppCompatActivity() {
     }
 
     private fun init() {
+
+        database = FirebaseDatabase.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        loadingDialogUtil = LoadingDialogUtil(this)
+
         binding.apply {
 
             val adapterCategory = ArrayAdapter(this@AddDestinationActivity, R.layout.item_category, destinationCategory)
@@ -54,6 +67,7 @@ class AddDestinationActivity : AppCompatActivity() {
                     tilDestinationVideoLink.error = "Isikan link video tempat wisata"
                 }
                 else -> {
+                    loadingDialogUtil.show()
                     submitDestination(
                         name = tieDestinationName.text.toString(),
                         description = tieDestinationDescription.text.toString(),
@@ -67,12 +81,34 @@ class AddDestinationActivity : AppCompatActivity() {
     }
 
     private fun submitDestination(name: String, description: String, category: String, price: String, video_link: String) {
-        val category = when (category){
+
+        if (auth.currentUser == null) return
+
+        val mCategory = when (category){
             "Wisata Alam" -> "alam"
             "Wisata Kota" -> "Kota"
             else -> "alam"
         }
 
+        val wisataModel = WisataModel(
+            uuid = auth.currentUser?.uid.toString(),
+            nama = name,
+            deskripsi = description,
+            kategory = mCategory,
+            harga = price,
+            video_link = video_link
+        )
 
+        database.reference.child("wisata").push()
+            .setValue(wisataModel)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    Toast.makeText(this, "Berhasil tambah wisata", Toast.LENGTH_SHORT).show()
+                    loadingDialogUtil.dismiss()
+                }else {
+                    Toast.makeText(this, "${task.exception}", Toast.LENGTH_SHORT).show()
+                    loadingDialogUtil.dismiss()
+                }
+            }
     }
 }
