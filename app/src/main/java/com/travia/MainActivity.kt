7a052ba3dcf89bb54fce59cca0f74f87496e92c1
DESKTOP-SQJUM6C\USER <yukimasaki2002@gmail.com>
2020-09-01 18:50:19 +0700

    fragment pesan selesai

--- app/src/main/java/com/travia/MainActivity.kt
+++ app/src/main/java/com/travia/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.travia
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
