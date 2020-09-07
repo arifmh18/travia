752ca5385921b57a77ddf168a1b3a4d972eed7d4
DESKTOP-SQJUM6C\USER <yukimasaki2002@gmail.com>
2020-09-07 20:57:27 +0700

    backend list pembayaran masih bisa pindah apps belum ke google play instal apps

--- app/src/main/java/com/travia/ListPembayaran.kt
+++ app/src/main/java/com/travia/ListPembayaran.kt
@@ -2,10 +2,26 @@ package com.travia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_list_pembayaran.*
 
 class ListPembayaran : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_pembayaran)
+        gopay.setOnClickListener {
+            val launchGojek = packageManager.getLaunchIntentForPackage("com.gojek.app")
+            startActivity(launchGojek)
+            finish()
+        }
+        dana.setOnClickListener {
+            val launchDana = packageManager.getLaunchIntentForPackage("id.dana")
+            startActivity(launchDana)
+            finish()
+        }
+        ovo.setOnClickListener {
+            val launchOvo = packageManager.getLaunchIntentForPackage("ovo.id")
+            startActivity(launchOvo)
+            finish()
+        }
     }
 }
