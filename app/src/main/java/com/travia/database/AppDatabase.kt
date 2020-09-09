44a0752b78f8a7f5988d4b1949a042877f3cf0d4
agus_prayogi <agus21apy@gmail.com>
2020-09-10 05:48:49 +0700

    create table pemesanan dan transaksi in Room

--- app/src/main/java/com/travia/database/AppDatabase.kt
+++ app/src/main/java/com/travia/database/AppDatabase.kt
@@ -4,13 +4,24 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.travia.PesanFragment
+import com.travia.database.dao.PemesananDao
+import com.travia.database.dao.TransaksiDao
 import com.travia.database.dao.WisataDao
+import com.travia.database.entity.PemesananEntity
+import com.travia.database.entity.TransaksiEntity
 import com.travia.database.entity.WisataEntity
 
-@Database(entities = [WisataEntity::class], version = 5, exportSchema = false)
+@Database(
+    entities = [WisataEntity::class, PemesananEntity::class, TransaksiEntity::class],
+    version = 5,
+    exportSchema = false
+)
 abstract class AppDatabase:RoomDatabase() {
 
-    abstract fun wisataDao():WisataDao
+    abstract fun wisataDao(): WisataDao
+    abstract fun transaksiDao(): TransaksiDao
+    abstract fun pemesananDao(): PemesananDao
 
     companion object {
         @Volatile
