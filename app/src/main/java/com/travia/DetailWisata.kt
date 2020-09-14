8a28c2ff5438d14ba5313419a93d57a64fabe6a1
DESKTOP-SQJUM6C\USER <yukimasaki2002@gmail.com>
2020-09-14 20:04:53 +0700

    no message

--- app/src/main/java/com/travia/DetailWisata.kt
+++ app/src/main/java/com/travia/DetailWisata.kt
@@ -2,10 +2,37 @@ package com.travia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.travia.R
+import com.travia.databinding.ActivityDetailWisataBinding
 
 class DetailWisata : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailWisataBinding
+    private lateinit var database: FirebaseDatabase
+
+    private lateinit var mMap: GoogleMap
+
+    private var id: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_wisata)
+        binding =ActivityDetailWisataBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+    private fun init(){
+
     }
 }
